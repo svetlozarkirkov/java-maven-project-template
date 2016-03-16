92c4f0d67bac78c200659f34850e05b2e2c42802
Svetlozar Kirkov <svetlozark@gmail.com>
2016-03-16 17:33:58 +0200

    cleaned app and apptest

--- src/main/java/org/sve/maventemplatepersonal/App.java
+++ src/main/java/org/sve/maventemplatepersonal/App.java
@@ -1,13 +1,8 @@
 package org.sve.maventemplatepersonal;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
     }
 }
